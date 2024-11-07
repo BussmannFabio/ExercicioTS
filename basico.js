0cef4984e3fdff1dfec73ea900c431453611ab07
BussmannFabio <126571737+BussmannFabio@users.noreply.github.com>
2024-11-07 11:50:29 -0300

    Add files via upload

---
+++ basico.js
@@ -0,0 +1,32 @@
+"use strict";
+// Desafios
+// Exercicio 1
+const mult = (valor) => valor * 2;
+console.log(mult(10));
+// Exercicio 2
+const dizerOla = (nome = 'Pessoa') => {
+    console.log('Olá, ' + nome);
+};
+dizerOla();
+dizerOla('Anna');
+// Exercicio 3
+const nums = [-3, 33, 38, 5];
+// Imprimir o menor valor
+console.log(Math.min(...nums));
+// Exercicio 4
+const array = [55, 20];
+array.push(...nums);
+// Adicionar todos os elementos de "nums" em array
+console.log(...array);
+// Exercicio 5
+const notas = [8.5, 6.3, 9.4];
+console.log(...notas);
+// Exercicio 6
+const cientista = {
+    primeiroNome: 'Will',
+    experiencia: 12
+};
+const { primeiroNome: name, experiencia: exp } = cientista;
+console.log(name);
+console.log(exp);
+//# sourceMappingURL=basico.js.map
